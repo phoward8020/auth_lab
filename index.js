f79a6853ab702946793f85d4557e3b8319273d33
Paul Howard <paul@paul-howard.net>
2014-12-11 08:25:21 -0800

    complete lab in class

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var session = require("express-session");
 var bodyParser = require('body-parser');
 var app = express();
+var bcrypt = require("bcrypt");
 
 var db = require('./models');
 
@@ -8,13 +10,31 @@ app.set('view engine','ejs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+    secret: 'thisIsAnOptionalButRecommendedValue',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+app.use(function(req, res, next) {
+    req.getUser = function() {
+        return req.session.user || false;
+    }
+    next();
+})
 
 app.get('/',function(req,res){
-    res.render('index',{user:false});
+    var user = req.getUser();
+    res.render('index',{user:user});
 });
 
 app.get('/restricted',function(req,res){
-    res.render('restricted');
+    if (req.getUser()) {
+        res.render('restricted');
+    } else {
+        res.send('DENIED!')
+    }
 });
 
 //login form
@@ -24,9 +44,31 @@ app.get('/auth/login',function(req,res){
 
 app.post('/auth/login',function(req,res){
     //do login here (check password and set session value)
-
+    db.user.find({where:{email:req.body.email}}).then(function(userObj) {
+        if (userObj) {
+            // check password now
+            // res.send("we will test password now.")
+            bcrypt.compare(req.body.password, userObj.password, function(err, match) {
+                if (match) {
+                    // store userObj in session here.
+                    // res.send("password is correct!");
+                    req.session.user = {
+                        id: userObj.id,
+                        email: userObj.email,
+                        name: userObj.name
+                    };
+                    res.redirect("/");
+                } else {
+                    res.send("invalid password!");
+                }
+            })
+        } else {
+            // error - user not found.
+            res.send("Unknown user.");
+        }
+    });
     //user is logged in forward them to the home page
-    res.redirect('/');
+    // res.redirect('/');
 });
 
 //sign up form
@@ -36,15 +78,36 @@ app.get('/auth/signup',function(req,res){
 
 app.post('/auth/signup',function(req,res){
     //do sign up here (add user to database)
-
-    //user is signed up forward them to the home page
-    res.redirect('/');
+    db.user.findOrCreate(
+    {
+        where: {
+            email:req.body.email
+        }, 
+        defaults: {
+            email:req.body.email, 
+            password:req.body.password, 
+            name:req.body.name
+        }
+    }).spread(function(user, created) {
+        if (!created) {
+            res.send('Email already exists in database')
+        } else {
+            res.send(user)
+            //user is signed up forward them to the home page
+            // res.redirect('/');
+        }
+    }).catch(function(error) {
+        res.send(error);
+    })
+    
 });
 
 //logout
 //sign up form
 app.get('/auth/logout',function(req,res){
-    res.send('logged out');
+    // res.send('logged out');
+    delete req.session.user;
+    res.redirect('/');
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,14 +1,33 @@
 "use strict";
 
+var bcrypt = require("bcrypt");
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [5, 200],
+            msg: "Password must be between 5 and 200 characters long."
+          }
+        }
+      },
     name: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    hooks: {
+      beforeCreate: function(data, garbage, callback) {
+        var passwordToEncrypt = data.password;
+        bcrypt.hash(passwordToEncrypt, 10, function(err, hash) {
+          data.password = hash;
+          callback(null, data);
+        })
       }
     }
   });
